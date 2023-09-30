de6fd7a1f09da08dceff84f84ab77d2ac14dbbe9
esti08-hu <98952769+esti08-hu@users.noreply.github.com>
2023-09-30 13:59:26 +0300

    Add files via upload

---
+++ javaScripte/main.js
@@ -0,0 +1,16 @@
+// SLIDE SECTION
+
+ var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      grapCursor:true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
